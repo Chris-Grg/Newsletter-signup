2b8282b6fff7cad2ec5241934a09d8d2d8c88347
Chris-Grg <chris.grg7@gmail.com>
2023-02-23 15:26:46 +0545

    hid api keys

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const request = require("request");
 const https = require("https");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
@@ -25,12 +26,15 @@ app.post("/", function (req, res) {
       },
     ],
   };
+  var apiKey = process.env.key;
+  var Name = process.env.Name;
   const jsonData = JSON.stringify(data);
   const url = "https://us21.api.mailchimp.com/3.0/lists/ca1d45ef64";
   const options = {
     method: "POST",
-    auth: "chris:aaedf11b21bcfc1dd1bab8a7f9b50e8b-us21",
+    auth: "Chris:" + apiKey, //doepo
   };
+  console.log(options);
   const request = https.request(url, options, function (response) {
     if (response.statusCode === 200) {
       res.sendFile(__dirname + "/success.html");
@@ -53,5 +57,3 @@ app.post("/success", function (req, res) {
 app.listen(process.env.PORT || 3000, function () {
   console.log("server running in port 3000");
 });
-
-// api_key = "aaedf11b21bcfc1dd1bab8a7f9b50e8b-us21";
